256edb1d4a2b462718b968a62ee218aa7462cbca
Leonardo Mazza <desenvolvedormazza@gmail.com>
2023-03-17 16:45:41 +0000

    chore: menu back to previous page

--- resources/views/admin/games/show.blade.php
+++ resources/views/admin/games/show.blade.php
@@ -1,6 +1,39 @@
 @extends('layouts.app')
 @section('content')
+    <div class="absolute top-0 right-0 mt-4 mr-4">
+        @if (Route::has('login'))
+            <div class="space-x-4">
+                @auth
+                @if(Auth::user()->is_admin) 
+                    <a
+                        href="{{ route('dashboard') }}"
+                        class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                    >
+                        Back to dashboard
+                    </a>
+                
+                @endif
+                    <a
+                        href="{{ route('logout') }}"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                        class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+                    >
+                        Log out
+                    </a>
 
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                @else
+                    <a href="{{ route('login') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div>
     <div class="flex flex-col justify-center items-center h-screen">
         <h1 class="text-3xl font-bold mb-4">{{$game->name}}</h1>
         <div class="mb-4">
